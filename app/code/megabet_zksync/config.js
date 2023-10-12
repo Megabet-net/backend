feb24f7a3bd928c2905a4cbaccaf0ef6f8b6d0f2
Albert Tran <alberttran0810@gmail.com>
2023-10-13 02:19:07 +0700

    Add module megabet_zksync

---
+++ app/code/megabet_zksync/config.js
@@ -0,0 +1,15 @@
+export default config = {
+    testnet: {
+        contract_address: {
+            megabet_token: "",
+            megabet_staking: "",
+            megabet_main: ""
+        },
+        network: {
+            url: "https://zksync2-testnet.zksync.dev",
+            ethNetwork: "goerli",
+            zksync: true,
+            verifyURL: "https://zksync2-testnet-explorer.zksync.dev/contract_verification"
+        }
+    }
+}
--- configs/zksync/index.js
+++
@@ -1,9 +0,0 @@
-export default config = {
-    testnet: {
-        contract_address: {
-            megabet_token: "",
-            megabet_staking: "",
-            megabet_main: ""
-        }
-    }
-}
